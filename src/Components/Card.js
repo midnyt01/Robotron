80681dd708b1a70509e8865e1f2d021f65f5dee1
Shivam <bishtshivam101@gmail.com>
2021-11-27 20:39:52 +0530

    final commit i guess

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Card = ({name, email, id}) => {
     return(
-    <div className = 'bg-light-green dib pa3 ma2 shadow-5 grow'>
+    <div className = 'bg-light-green dib pa3 ma2 shadow-5 grow br3'>
         <img alt='' src={`https://robohash.org/${ id }`} />
         <div className='tc'>
             <h2>{ name }</h2>
