779667dc3a4dd863f7ab618f999855e2c4bd83d3
Shivam <bishtshivam101@gmail.com>
2021-11-27 11:00:38 +0530

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Card = ({name, email, id}) => {
+    return(
+    <div className = 'bg-light-green dib pa3 ma2 shadow-5 grow'>
+        <img alt='' src={`https://robohash.org/${ id }`} />
+        <div className='tc'>
+            <h2>{ name }</h2>
+            <p>{ email }</p>
+        </div>
+    </div>
+    )
+}
+
+
+export default Card;
